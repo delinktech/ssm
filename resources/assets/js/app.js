c938ed812fcf1e13baebcc27149d817c18c9112e
Unknown <kamaunewton78@gmail.com>
2018-03-17 15:50:20 +0300

    Creating route components for vue in app.vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,6 +5,26 @@ import VueMaterial from 'vue-material';
 Vue.use(VueMaterial);*/
 
 Vue.component(
+  'school-component',
+  require('./components/school.component/SchoolComponent.vue')
+);
+
+Vue.component(
+  'result-component',
+  require('./components/results.component/ResultsComponent.vue')
+);
+
+Vue.component(
+  'teacher-component',
+  require('./components/teachers.component/TeachersComponent.vue')
+);
+
+Vue.component(
+  'student-component',
+  require('./components/students.component/StudentsComponent.vue')
+);
+
+Vue.component(
   'app-component',
   require('./components/AppComponent.vue')
 );
