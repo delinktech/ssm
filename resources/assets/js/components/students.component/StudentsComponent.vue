a3e45c146b33b8f534514e8769658743e0f5de28
Delink-D <delinkdesigns@gmail.com>
2018-03-17 23:15:24 +0300

    setup of students view component

--- resources/assets/js/components/students.component/StudentsComponent.vue
+++ resources/assets/js/components/students.component/StudentsComponent.vue
@@ -1,1 +1,47 @@
-StudentComponent.vue
+<template>
+  <div id="view-students">
+    <h1>View all students</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'StudentsComponent',
+    data() {
+      return {
+        students: [],
+        student: {
+          student_reg: '',
+          student_firstName: '',
+          student_secondName: '',
+          student_surName: '',
+          student_gender: '',
+          student_parent: '',
+          student_class: '',
+          student_dob: '',
+        },
+        student_id: '',
+        pagination: {},
+        edit: false
+      }
+    },
+
+    created() {
+      this.fetchStudents();
+    },
+
+    methods: {
+      fetchStudents() {
+        fetch('api/students')
+        .then(res => res.json())
+        .then(res => {
+          console.log(res.data);
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
