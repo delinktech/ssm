23e0324c48ec6c3219c19de301142ad96cc88ddc
Delink-D <delinkdesigns@gmail.com>
2018-03-17 22:04:45 +0300

    finalized on students CRUD trough api

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -40,7 +40,22 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      // store students in the databse
+      $student = $request->isMethod('put') ? Student::findOrFail($request->student_id) : new Student;
+
+      $student->id = $request->input('student_id');
+      $student->student_reg = $request->input('student_reg');
+      $student->class_id = $request->input('class_id');
+      $student->student_first_name = $request->input('student_first_name');
+      $student->student_second_name = $request->input('student_second_name');
+      $student->student_last_surname = $request->input('student_last_surname');
+      $student->student_dob = $request->input('student_dob');
+      $student->student_gender = $request->input('student_gender');
+      $student->student_parent = $request->input('student_parent');
+
+      if ($student->save()) {
+        return new StudentResource($student);
+      }
     }
 
     /**
@@ -89,6 +104,12 @@ class StudentController extends Controller
      */
     public function destroy($id)
     {
-        //
+      // delete student
+      $student = Student::findOrFail($id);
+
+      if ($student->delete()) {
+        // return sngle student as a resource
+        return new StudentResource($student);
+      }
     }
 }
