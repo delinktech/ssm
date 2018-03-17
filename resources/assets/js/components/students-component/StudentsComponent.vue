35922b1a7c769479a2eb9aef98fb24f7eb771fca
Delink-D <delinkdesigns@gmail.com>
2018-03-18 02:20:51 +0300

    fetch all students to the ui from api

--- resources/assets/js/components/students-component/StudentsComponent.vue
+++ resources/assets/js/components/students-component/StudentsComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="view-students">
     <h1>View all students</h1>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -36,6 +37,12 @@
         .then(res => res.json())
         .then(res => {
           console.log(res.data);
+
+          // assign the data to students array
+          this.students = res.data;
+        })
+        .catch(err => {
+          console.log('Error: Getting students', err);
         })
       }
     }
