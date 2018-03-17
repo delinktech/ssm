a9f18bdd9e90a552e972aded2173122ea58b7352
Delink-D <delinkdesigns@gmail.com>
2018-03-17 11:10:19 +0300

    students table migration

---
+++ database/factories/StudentsFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Student::class, function (Faker $faker) {
+    return [
+        'student_reg' => $faker->unique()->text(10),
+        'class_id' => $faker->numberBetween(1-2),
+        'student_first_name' => $faker->firstName,
+        'student_second_name' => $faker->lastName,
+        'student_last_surname' => $faker->name,
+        'student_dob' => $faker->date('Y-m-d'),
+        'student_gender' => $faker->randomElement(['male', 'female']),
+        'student_parent' => $faker->numberBetween(1-2)
+    ];
+});
---
+++ database/migrations/2018_03_17_061410_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('student_reg');
+            $table->integer('class_id');
+            $table->string('student_first_name');
+            $table->string('student_second_name');
+            $table->string('student_last_surname');
+            $table->date('student_dob');
+            $table->string('student_gender');
+            $table->integer('student_parent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
