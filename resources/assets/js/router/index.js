a7e40e79e9c829346ec1a62123ca5f54c561a05f
Delink-D <delinkdesigns@gmail.com>
2018-03-18 02:18:49 +0300

    created the vuejs router

---
+++ resources/assets/js/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import AppComponent from '../components/app-component/AppComponent'
+
+import StudentsComponent from '../components/students-component/StudentsComponent'
+import StudentAddComponent from '../components/students-component/add/StudentAddComponent'
+import StudentEditComponent from '../components/students-component/edit/StudentEditComponent'
+
+Vue.use(Router)
+
+let router = new Router({
+  routes: [
+    {
+      path: '*',
+      redirect: '/dashboard'
+    },
+    {
+      path: '/',
+      redirect: '/dashboard'
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard-component'
+      // component: AppComponent
+    },
+    {
+      path: '/students',
+      name: 'students-component',
+      component: StudentsComponent,
+      children: [
+        {
+          path: 'edit',
+          component: StudentEditComponent
+        },
+        {
+          path: 'add',
+          component: StudentAddComponent
+        }
+      ]
+    }
+  ]
+})
+
+export default router 
