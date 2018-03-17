92a3ce0dc0e5408c9532c1d5ebc1fd68d08730e7
Delink-D <delinkdesigns@gmail.com>
2018-03-17 11:07:03 +0300

    add the routes for students in the api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// list students
+Route::get('students', 'StudentController@index');
+
+// show single student
+Route::get('student/{id}', 'StudentController@show');
+
+// add a student
+Route::post('student', 'StudentController@store');
+
+// update/edit a student
+Route::put('student', 'StudentController@store');
+
+// delete student
+Route::delete('student/{id}', 'StudentController@destroy');
