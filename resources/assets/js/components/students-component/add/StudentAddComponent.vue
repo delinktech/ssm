4bb624e99585db7e635b34437c9617b9e033fd95
Delink-D <delinkdesigns@gmail.com>
2018-03-18 02:19:33 +0300

    created the edit and add students on their own sub components

---
+++ resources/assets/js/components/students-component/add/StudentAddComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="add-student">
+
+    <h2>Add student</h2>
+    <form novalidate class="md-layout" @submit.prevent="validateUser">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Users</div>
+        </md-card-header>
+      </md-card>
+
+      <md-button class="md-primary">Primary</md-button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+   export default {
+    name: 'StudentAddComponent',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/students-component/edit/StudentEditComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="edit-student">
+    <h2>Editing student</h2>
+  </div>
+</template>
+
+<script>
+   export default {
+    name: 'StudentEditComponent',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
