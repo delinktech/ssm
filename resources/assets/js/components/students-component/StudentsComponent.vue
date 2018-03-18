fbab36b3ee65bb4f18b6b58673dd57a36000de3c
Delink-D <delinkdesigns@gmail.com>
2018-03-18 10:03:58 +0300

    moved the functionality to the list of patients component

--- resources/assets/js/components/students-component/StudentsComponent.vue
+++ resources/assets/js/components/students-component/StudentsComponent.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="view-students">
-    <h1>View all students</h1>
     <router-view></router-view>
   </div>
 </template>
@@ -10,40 +9,16 @@
     name: 'StudentsComponent',
     data() {
       return {
-        students: [],
-        student: {
-          student_reg: '',
-          student_firstName: '',
-          student_secondName: '',
-          student_surName: '',
-          student_gender: '',
-          student_parent: '',
-          student_class: '',
-          student_dob: '',
-        },
-        student_id: '',
-        pagination: {},
-        edit: false
       }
     },
 
     created() {
-      this.fetchStudents();
+      
     },
 
     methods: {
       fetchStudents() {
-        fetch('api/students')
-        .then(res => res.json())
-        .then(res => {
-          console.log(res.data);
 
-          // assign the data to students array
-          this.students = res.data;
-        })
-        .catch(err => {
-          console.log('Error: Getting students', err);
-        })
       }
     }
   }
