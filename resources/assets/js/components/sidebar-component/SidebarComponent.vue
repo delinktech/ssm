facec29a5c2f8aa1afedd4f8beb1d4050f63b345
Delink-D <delinkdesigns@gmail.com>
2018-03-18 23:14:37 +0300

    updated the navigation

--- resources/assets/js/components/sidebar-component/SidebarComponent.vue
+++ resources/assets/js/components/sidebar-component/SidebarComponent.vue
@@ -5,19 +5,19 @@
     <section class="sidebar">
 
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel">
+      <!-- <div class="user-panel">
         <div class="pull-left image">
           <img :src="pictureUrl" />
         </div>
         <div class="pull-left info">
           <div>
-            <p class="white">{{ displayName }}</p>
+            <p class="white">Delink</p>
           </div>
           <a href="javascript:;">
             <i class="fa fa-circle text-success"></i> Online
           </a>
         </div>
-      </div>
+      </div> -->
 
       <!-- Sidebar Menu -->
       <sidebar-menu />
@@ -31,7 +31,7 @@
 
   export default {
     name: 'Sidebar',
-    props: ['displayName', 'pictureUrl'],
+    // props: ['displayName', 'pictureUrl'],
     components: { SidebarMenu },
   }
 </script>
