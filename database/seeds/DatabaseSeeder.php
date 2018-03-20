89d4306392a37e96cf0ab60304cc5dd01044a325
Delink-D <delinkdesigns@gmail.com>
2018-03-20 23:21:27 +0300

    added the techers db to seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $this->call(StudentsTableSeeder::class);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->call(StudentsTableSeeder::class);
+    $this->call(TeachersTableSeeder::class);
+  }
 }
