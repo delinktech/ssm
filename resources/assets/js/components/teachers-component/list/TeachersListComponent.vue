bb125134856dcb3665808a076a9044cc479b6a72
Delink-D <delinkdesigns@gmail.com>
2018-03-20 23:20:49 +0300

    displayed the teachers on the UI

--- resources/assets/js/components/teachers-component/list/TeachersListComponent.vue
+++ resources/assets/js/components/teachers-component/list/TeachersListComponent.vue
@@ -1,7 +1,40 @@
 <template>
 	<div id="teachers-list">
-		<!-- TODO: display list of teachers here -->
-    <h3>LIST OF TEACHERS ;)</h3>
+		<h2>List of all Teachers</h2>
+    <md-table>
+    <!-- {{teachers}} -->
+      <md-table-row>
+          <md-table-head>No.</md-table-head>
+          <md-table-head>Registration</md-table-head>
+          <md-table-head>Name</md-table-head>
+          <md-table-head>Last Name</md-table-head>
+          <md-table-head>Email</md-table-head>
+          <md-table-head>Id No</md-table-head>
+          <md-table-head>Actions</md-table-head>
+      </md-table-row>
+      <md-table-row v-for="teacher, index in teachers" :key="teacher.id">
+        <md-table-cell>{{++index}}</md-table-cell>
+        <md-table-cell>{{teacher.teacher_code}}</md-table-cell>
+        <md-table-cell>{{teacher.first_name}}</md-table-cell>
+        <md-table-cell>{{teacher.teacher_surname}}</md-table-cell>
+        <md-table-cell>{{teacher.teacher_email}}</md-table-cell>
+        <md-table-cell>{{teacher.teacher_id_number}}</md-table-cell>
+
+        <md-table-cell>
+          <md-button @click="editStudent(teacher.id)" class="md-icon-button">
+            <md-icon>edit</md-icon>
+          </md-button>
+          <md-button @click="deleteTeacher(teacher.id)" class="md-icon-button">
+            <md-icon>delete</md-icon>
+          </md-button>
+        </md-table-cell>
+      </md-table-row>
+    </md-table>
+
+    <md-snackbar :md-position="position" :md-duration="4000" :md-active.sync="showSnackbar" md-persistent>
+      <span>ERROR: Getting list of Teachers</span>
+      <md-button class="md-primary" @click="fetchTeachers()">Retry</md-button>
+    </md-snackbar>
 	</div>
 </template>
 
@@ -10,7 +43,12 @@
     name: 'TeachersListComponent',
     data: () => {
       return {
-        teachers: []
+        teachers: [],
+        _id: '',
+        pagination: {},
+        edit: false,
+        showSnackbar: false,
+        position: 'center',
       }
     },
 
@@ -24,7 +62,9 @@
         fetch('api/teachers')
         .then(res => res.json())
         .then(res => {
-          this.teachers = res;
+          // all good
+          console.log('results', res);
+          this.teachers = res.data;
         })
         .catch(err => {
           console.log('ERROR: getting teachers ->', err);
