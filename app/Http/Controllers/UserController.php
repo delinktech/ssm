7da5792f745e6f49bc7b3c136c8fcbbc675b35f7
Delink-D <delinkdesigns@gmail.com>
2018-03-28 01:06:45 +0300

    added a function to signin through api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+  /*
+    function to signup users to the app
+  */
   public function signup(Request $request) {
+    // validating the forms data
     $this->validate($request, [
-      'name' => 'required',
-      'email' => 'required|email|unique:users',
-      'password' => 'required'
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required'
       ]);
 
     $user = new User([
@@ -26,5 +32,35 @@ class UserController extends Controller
       'message' => 'Sussesfuly added User'
     ], 201);
   }
+
+  /*
+    function to sigin users to the app
+  */ 
+    public function signin(Request $request) {
+      // validating the forms data
+      $this->validate($request, [
+          'name' => 'required',
+          'email' => 'required|email',
+          'password' => 'required'
+        ]);
+
+      $credentials = $request->only('email', 'password');
+
+      try {
+        if (!$token = JWTAuth::attempt($credentials)) {
+          return response()->json([
+            'error' => 'Invalid Credentials!'
+            ], 401);
+        }
+      } catch (JWTException $e) {
+        return response()->json([
+          'error' => 'Could not create Token!'
+          ], 500);
+      }
+
+      // succss creating token return the token
+      return response()->json([
+        'token' => $token
+        ], 200); 
+    }
 }
-// App\Http\Controllers\Auth\UserController does not exist
