e2e1279a7a838644ed16059981d2dfc02533dac9
Delink-D <delinkdesigns@gmail.com>
2018-03-28 01:18:12 +0300

    resolved merge conflicts in students migration

--- database/migrations/2018_03_17_061410_create_students_table.php
+++ database/migrations/2018_03_17_061410_create_students_table.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
@@ -6,41 +6,36 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateStudentsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('students', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->increments('student_id');
-            $table->string('student_reg')->unique();
-=======
-            $table->increments('id');
-            $table->string('student_reg');
->>>>>>> master
-            $table->integer('class_id');
-            $table->string('student_first_name');
-            $table->string('student_second_name');
-            $table->string('student_last_surname');
-            $table->date('student_dob');
-            $table->date('reg_date');
-            $table->string('student_gender');
-            $table->longText('passport_photo')->nullable();
-            $table->string('student_school');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('students', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('student_reg')->unique();
+      $table->integer('class_id');
+      $table->string('student_first_name');
+      $table->string('student_second_name');
+      $table->string('student_last_surname');
+      $table->date('student_dob');
+      $table->date('reg_date');
+      $table->string('student_gender');
+      $table->longText('passport_photo')->nullable();
+      $table->string('student_school');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('students');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('students');
+  }
 }
