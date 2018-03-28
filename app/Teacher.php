c56912208cf293b5532ffe975d5406969026514c
Delink-D <delinkdesigns@gmail.com>
2018-03-28 19:24:58 +0300

    changes to the the models

--- app/Teacher.php
+++ app/Teacher.php
@@ -6,23 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
 {
-    // private $table = ('teachers');
+  protected $table = 'teachers';
 
-    //  protected $table='tables';
+  protected $primaryKey='id';
 
-    // protected $primaryKey='id';
+  protected $casts = [
+    'subjects_id' => 'array'
+  ];
 
-    // protected $fillable = ['teacher_surname','teacher_id_number','teacher_email','teacher_email','teacher_dev_reg','teacher_api_session','teacher_password','teacher_status','teacher_school_id','teacher_class_id','subjects_id','teacher_user_id','teacher_code';
+  protected $fillable = ['teacher_username','teacher_surname','teacher_id_number','teacher_email','teacher_email','teacher_dev_reg','teacher_api_session','teacher_password','teacher_status','teacher_school_id','teacher_class_id','subjects_id','teacher_code'
+  ];
 
-    // ]
+  // database relationship 
+  public function studentTeacher(){
+    return $this->hasMany(studentTeacher::class,'student_id','id');
+  } 
 
-    //database relationship 
-    //  public function studentTeacher(){
-    //     return $this->hasMany(studentTeacher::class,'student_id','id');
-    // } 
-    // public function schoolTeacher(){
-    //     return $this->hasOne(schoolTeacher::class,'school_id','id');
-    // }
+  public function schoolTeacher(){
+    return $this->hasOne(schoolTeacher::class,'school_id','id');
+  }
 }
-
-
