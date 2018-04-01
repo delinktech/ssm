7d300110490e3258401ee86b9d64bf5098be167f
Delink-D <delinkdesigns@gmail.com>
2018-04-01 15:32:07 +0300

    included the jwtAuth file to the app

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,5 +58,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'cors' => \App\Http\Middleware\Cors::class, 
+        'auth.jwt' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Http\Middleware\RefreshToken::class
     ];
 }
