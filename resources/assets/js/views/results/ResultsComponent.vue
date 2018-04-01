10cfdb26b493bc03c78213a1fdf183de722b0596
Delink-D <delinkdesigns@gmail.com>
2018-04-02 02:14:18 +0300

    setup of results component

---
+++ resources/assets/js/views/results/ResultsComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+    <h3>Students Results</h3>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
