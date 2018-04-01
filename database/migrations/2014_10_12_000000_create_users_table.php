db48157a92e836394202380a1a5db95fcb4ddc5f
Delink-D <delinkdesigns@gmail.com>
2018-04-02 00:01:35 +0300

    update to users/schools tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,32 +6,44 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('username');
-            $table->string('name');
-            $table->string('roles');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->increments('id');
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+      $table->string('user_firstname');
+      $table->string('user_lastname');
+      $table->string('user_phone');
+
+      $table->string('username')->unique();
+      $table->string('email')->unique();
+      $table->string('password');
+
+      $table->string('school');
+      $table->string('approved');
+      $table->string('roles');
+      $table->string('level');
+      
+      $table->rememberToken();
+
+      $table->string('creatdBy');
+      $table->string('updatedBy');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 }
--- database/migrations/2018_03_17_114456_create_schools_table.php
+++ database/migrations/2018_03_17_114456_create_schools_table.php
@@ -6,38 +6,44 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateSchoolsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('schools', function (Blueprint $table) {
-            $table->increments('id');
-             $table->text('school_id');
-             $table->text('school_name');
-             $table->integer('reg_no');
-             $table->integer('class_id');
-            $table->text('school_logo')->nullable();
-            $table->text('school_address');
-            $table->text('school_mobile');
-            $table->text('school_email');
-            $table->integer('county_id');
-            $table->text('school_town');
-            $table->integer('level');
-            $table->integer('user_id');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('schools', function (Blueprint $table) {
+      $table->increments('id');
+      
+      $table->text('school_code');
+      $table->text('school_name');
+      $table->integer('school_level');
+      $table->integer('school_type');
+      $table->text('school_gender');
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('schools');
-    }
+      $table->text('school_county');
+      $table->text('school_sub_county');
+      $table->text('school_ward');
+
+      $table->text('school_phone');
+      $table->text('school_email');
+
+      $table->integer('createdBy');
+      $table->text('updatedBy');
+
+      $table->text('school_logo')->nullable();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('schools');
+  }
 }
