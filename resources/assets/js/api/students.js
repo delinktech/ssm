846048e23f49af79497e6e56096c0f7dec8a8fb7
Delink-D <delinkdesigns@gmail.com>
2018-04-01 22:31:10 +0300

    amend students api reuest to pass the token

--- resources/assets/js/api/students.js
+++ resources/assets/js/api/students.js
@@ -1,13 +1,10 @@
 // import request from '../utils/request'
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import { getToken } from '../utils/auth'
 
 Vue.use(VueResource)
 
 export const getStudents = () => {
-  return Vue.http.get('http://dev.ssm.io/api/students')
-  // return fetch({
-  //   url: 'dev.ssm.io/api/students',
-  //   method: 'get'
-  // })
+  return Vue.http.get('/api/students', { params: { 'token': getToken() }} )
 }
