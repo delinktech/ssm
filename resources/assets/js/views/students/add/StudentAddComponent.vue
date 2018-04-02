3355d8cca32b56313594c7593dc704f8745f1c70
Delink-D <delinkdesigns@gmail.com>
2018-04-03 01:46:14 +0300

    students add form

--- resources/assets/js/views/students/add/StudentAddComponent.vue
+++ resources/assets/js/views/students/add/StudentAddComponent.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="app-container">
-    add students
+    <h3>Add Student</h3>
+
+    <!-- form starts -->
+    <el-form ref="form" :model="studentForm">
+
+      <el-col :span="7">
+        <el-form-item label="First Name">
+          <el-input v-model="studentForm.firstName"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="1">&nbsp;</el-col>
+      <el-col :span="7">
+        <el-form-item label="Second Name">
+          <el-input v-model="studentForm.secondName"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="1">&nbsp;</el-col>
+      <el-col :span="7">
+        <el-form-item label="Last Name">
+          <el-input v-model="studentForm.lastName"></el-input>
+        </el-form-item>
+      </el-col>
+
+      <el-col :span="24">
+        <el-button type="success" style="margin-top: 12px;" @click="onSubmit(form)">Save Student</el-button>
+      </el-col>
+
+    </el-form> <!-- /form ends -->
+
   </div>
 </template>
 
