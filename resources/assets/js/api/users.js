44d0c53fc078bb33c47bd3f6e078bcc2bf053f8f
Delink-D <delinkdesigns@gmail.com>
2018-04-03 00:40:23 +0300

    function to call the api for users

---
+++ resources/assets/js/api/users.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import { getToken } from '../utils/auth'
+
+Vue.use(VueResource)
+
+/* function to add/signup users to the app */
+export const saveUser = () => {
+  return Vue.http.post('/api/user', { params: { 'token': getToken() }} )
+}
+
+/* function to fetch all the users from the api */
+export const getUsers = () => {
+  return Vue.http.get('/api/user/list', { params: { 'token': getToken() }} )
+}
+
