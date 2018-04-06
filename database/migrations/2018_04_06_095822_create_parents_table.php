cb33274ad09900b59eaefcd973da97c268267714
Unknown <kamaunewton78@gmail.com>
2018-04-06 13:23:13 +0300

    created parents table and put all the fields together with the parents model

---
+++ database/migrations/2018_04_06_095822_create_parents_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('parent_first_name');
+            $table->string('parent_second_name');
+            $table->integer('parent_national_id');
+            $table->integer('parent_Phone_number');
+            $table->string('parent_email');
+            $table->integer('parent_student_id');
+            $table->string('parent_county');
+            $table->string('parent_sub_county');
+            $table->string('parent_ward');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parents');
+    }
+}
