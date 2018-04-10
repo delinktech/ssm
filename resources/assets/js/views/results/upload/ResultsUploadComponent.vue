a284a8ff628758f24922ba54ce30153875ad978b
Delink-D <delinkdesigns@gmail.com>
2018-04-10 18:29:19 +0300

    add comments to code

--- resources/assets/js/views/results/upload/ResultsUploadComponent.vue
+++ resources/assets/js/views/results/upload/ResultsUploadComponent.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="app-container">
+    <!-- upload excel files here -->
     <upload-excel-component @on-selected-file='selected'></upload-excel-component>
+
+    <!-- display data on this table -->
     <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
       <el-table-column v-for='item of tableHeader' :prop="item" :label="item" :key='item'>
       </el-table-column>
     </el-table>
+    
   </div>
 </template>
 
