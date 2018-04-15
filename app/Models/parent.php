3a970b7fe83e606b3f0add813fcb437563f4f339
Delink-D <delinkdesigns@gmail.com>
2018-04-15 19:53:32 +0300

    inlude all fields of parent in the model

--- app/Models/parent.php
+++ app/Models/parent.php
@@ -10,6 +10,8 @@ class parent extends Model
 
   protected $primaryKey='id';
 
+  protected $fillable = ['parent_first_name','parent_second_name','parent_national_id','parent_Phone_number','parent_email','parent_county','parent_sub_county','parent_ward'];
+
   public function parentStudent(){
     return $this->hasMany(studentTeacher::class,'student_id','id' 'forign_key');
   }
--- database/migrations/2018_04_06_095822_create_parents_table.php
+++ database/migrations/2018_04_06_095822_create_parents_table.php
@@ -15,12 +15,12 @@ class CreateParentsTable extends Migration
     {
         Schema::create('parents', function (Blueprint $table) {
             $table->increments('id');
+            
             $table->string('parent_first_name');
             $table->string('parent_second_name');
             $table->integer('parent_national_id');
             $table->integer('parent_Phone_number');
             $table->string('parent_email');
-            $table->integer('parent_student_id');
             $table->string('parent_county');
             $table->string('parent_sub_county');
             $table->string('parent_ward');
