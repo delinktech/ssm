39f0afdb11568f40a707b91ca2df67a8e6f6c549
Delink-D <delinkdesigns@gmail.com>
2018-04-18 19:41:16 +0300

    included all the api request for teacher object

--- resources/assets/js/api/teachers.js
+++ resources/assets/js/api/teachers.js
@@ -5,6 +5,22 @@ import { getToken } from '../utils/auth'
 
 Vue.use(VueResource)
 
+/* get all the teachers through the api */
 export const getTeachers = () => {
   return Vue.http.get('/api/teachers', { params: { 'token': getToken() }} )
+}
+
+/* get a single teacher through the api */
+export const getTeacher = (_id) => {
+  return Vue.http.get(`/api/teacher/${_id}`, { params: {'token': getToken() }} )
+}
+
+/* edit a single teacher through the api */
+export const updateTeacher = (formData) => {
+  return Vue.http.put('/api/teacher', formData, { params: {'token': getToken() }} )
+}
+
+/* delete a teacher through the api */
+export const deleteTeacher = (_id) => {
+  return Vue.http.delete(`/api/teacher/${_id}`, { params: {'token': getToken() }} )
 }
