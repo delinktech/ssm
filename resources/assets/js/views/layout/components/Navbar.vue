ff942e3517ba04042ba6675cf5305754f8afb1f3
Delink-D <delinkdesigns@gmail.com>
2018-04-18 19:45:50 +0300

    renamed from home to dashboard

--- resources/assets/js/views/layout/components/Navbar.vue
+++ resources/assets/js/views/layout/components/Navbar.vue
@@ -10,7 +10,7 @@
       <el-dropdown-menu class="user-dropdown" slot="dropdown">
         <router-link class="inlineBlock" to="/">
           <el-dropdown-item>
-            Home
+            Dashboard
           </el-dropdown-item>
         </router-link>
         <el-dropdown-item divided>
