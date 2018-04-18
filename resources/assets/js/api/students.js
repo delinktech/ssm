c924e02903e0b057f52ba11b3f72388f582718b0
Delink-D <delinkdesigns@gmail.com>
2018-04-18 20:23:53 +0300

    updated the api requests for student

--- resources/assets/js/api/students.js
+++ resources/assets/js/api/students.js
@@ -4,22 +4,27 @@ import { getToken } from '../utils/auth'
 
 Vue.use(VueResource)
 
-/* fetch all users function via api */
+/* fetch all students function via api */
 export const getStudents = () => {
   return Vue.http.get('/api/students', { params: { 'token': getToken() }} )
 }
 
+/* fetch single student function via api */
+export const getStudent = (_id) => {
+  return Vue.http.get(`/api/student/${_id}`, { params: { 'token': getToken() }} )
+}
+
 /* save student through the api */
 export const saveStudent = () => {
-  return Vue.http.post('/api/students', { params: { 'token': getToken() }} )
+  return Vue.http.post('/api/student', { params: { 'token': getToken() }} )
 }
 
 /* save edits of student through the api */
 export const saveEditStudent = () => {
-  return Vue.http.put('/api/students', { params: { 'token': getToken() }} )
+  return Vue.http.put('/api/student', { params: { 'token': getToken() }} )
 }
 
 /* delete a student through the api */
 export const deleteStudent = (_id) => {
-  return Vue.http.delete('/api/students', { params: {'id': _id, 'token': getToken() }} )
+  return Vue.http.delete(`/api/student/${_id}`, { params: {'token': getToken() }} )
 }
