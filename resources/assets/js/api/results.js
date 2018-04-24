e0e2382b5d45a6e50e2a46b5e7a99564c823ec77
Delink-D <delinkdesigns@gmail.com>
2018-04-24 23:00:22 +0300

    udate the results request api paths

--- resources/assets/js/api/results.js
+++ resources/assets/js/api/results.js
@@ -9,17 +9,22 @@ export const getResults = () => {
   return Vue.http.get('/api/results', { params: { 'token': getToken() }} )
 }
 
+/* fetch results by student through api */
+export const getResults = (_id) => {
+  return Vue.http.get(`/api/results/student/${_id}`, { params: { 'token': getToken() }} )
+}
+
 /* save result through the api */
-export const saveResult = () => {
-  return Vue.http.post('/api/result', { params: { 'token': getToken() }} )
+export const saveUpload = (formData) => {
+  return Vue.http.post('/api/results', formData, { params: { 'token': getToken() }} )
 }
 
 /* save edits of parent through the api */
-export const saveEditResult = () => {
-  return Vue.http.put('/api/results', { params: { 'token': getToken() }} )
+export const saveEditResult = (formData) => {
+  return Vue.http.put('/api/results', formData, { params: { 'token': getToken() }} )
 }
 
 /* delete a parent through the api */
 export const deleteResult = (_id) => {
-  return Vue.http.delete('/api/Result', { params: {'id': _id, 'token': getToken() }} )
+  return Vue.http.delete(`/api/result/${_id}`, { params: {'token': getToken() }} )
 }
