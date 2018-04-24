6713a4228691890b4d3985ae77fe498c1a8fc412
Delink-D <delinkdesigns@gmail.com>
2018-04-24 23:01:26 +0300

    add a fab button to upload results

--- resources/assets/js/views/results/upload/ResultsUploadComponent.vue
+++ resources/assets/js/views/results/upload/ResultsUploadComponent.vue
@@ -3,6 +3,9 @@
     <!-- upload excel files here -->
     <upload-excel-component @on-selected-file='selected'></upload-excel-component>
 
+    <md-button @click="getData(tableData)" class="md-fab md-fab-bottom-right md-primary" id="fab-upload">
+      <md-icon>file_upload</md-icon>
+    </md-button>
     <!-- display data on this table -->
     <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
       <el-table-column v-for='item of tableHeader' :prop="item" :label="item" :key='item'>
@@ -28,6 +31,9 @@ export default {
     selected(data) {
       this.tableData = data.results
       this.tableHeader = data.header
+    },
+    getData(data) {
+      console.log('data', data.length)
     }
   }
 }
