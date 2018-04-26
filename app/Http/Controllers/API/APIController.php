5ab6511644b612a1addf8a02910735955b504ded
Unknown <kamaunewton78@gmail.com>
2018-04-26 16:09:12 +0300

    This is where our API handling will take place

---
+++ app/Http/Controllers/API/APIController.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Model\Student
+use App\Model\Teacher
+use App\Model\Results
+use App\Model\School
+use App\Http\Controllers\StudentController
+use App\Http\Controllers\TeacherController
+use App\Http\Controllers\ResultsController
+use App\Http\Controllers\SchoolController
+
+
