58f314655cc8eee06d1c9a9fcf2a3476a537e898
Delink-D <delinkdesigns@gmail.com>
2018-04-29 02:20:36 +0300

    removed un-necessary code in students

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -24,15 +24,10 @@ class StudentController extends Controller
      */
     public function index()
     {
-      // $data["edit"] = $id;
-      //   if (Auth::user()->teacher_username == 0) {
-      //       $data['students'] = Student::where('school_id', $this->school_id->id)->orderBy('id', 'desc')->get();
-      //   } else if (Auth::user()->user_id == 2) {
-      //       $data['students'] = $this->getTeacherStudents();
-      //   }
-      $students = Student::Paginate(15);
+      // TODO: only get students by school of logedin user
+      $students = Student::Paginate(30);
+
       // return a collection of students
-      // return view('students', 'StudentController@index');
       return StudentResource::collection($students);
     }
 
@@ -43,106 +38,7 @@ class StudentController extends Controller
      */
     public function create()
     {
-        $this->validate($request, [
-            'first_name' => 'required',
-            'reg_no' => 'required|unique:students,reg_no,NULL,id,organisation_info,' . $this->organisation_info->id,
-            'surname' => 'required',
-            'gender' => 'required',
-            'dob' => 'required',
-            'reg_date' => 'required',
-            'student_gender' => 'required',
-            'passport_photo' => 'required',
-            'student_school' => 'required'
-        ]);
-         $student = Student::create([
-            'first_name' => $request->first_name,
-            'surname' => $request->surname,
-            'reg_no' => $request->reg_no,
-            'adm_date' => new \DateTime($request->adm_date),
-            'status' => Constants::ACTIVE_STUDENT,
-            'parent_id' => 0,
-            'passport_photo' => $request->passport_photo,
-            'gender' => $request->gender,
-            'school_id' => $this->organisation_info->id
-        ]);
-          return response()->json([
-            'success' => true,
-            'id' => $student->id
-        ]);
-    }
-     public function multipleCreate(Request $request)
-    {
-        $template = "imports-" . date('YmdHiiss') . "." . $request->template->getClientOriginalExtension();
-        $uploaded = $request->template->move(public_path('student-imports'), $template);
-        //$tmp_name=UploadedFile::createFromBase($request->file('file'));
-
-        // return $uploaded;
-
-        $upload_file = asset($uploaded);
-
-        // return $upload_file;
-
-        $xlsx = new SimpleXLSX(file_get_contents($uploaded), true, true);
-
-
-        list($num_cols, $num_rows) = $xlsx->dimension(1);
-        $f = 0;
-        $students_added = 0;
-        $data = [];
-        foreach ($xlsx->rows() as $r) {
-
-            // Ignore the inital name row of excel file
-            if ($f < 3) {
-                $f++;
-                continue;
-            }
-
-            $student = new Student();
-            if ($r[1] == "" || $r[3] == "") {
-                continue;
-            }
-            $student->surname = "" . $r[0];
-            $student->first_name = "" . $r[1];
-            $date_array = date_parse($r[2]);
-            $date_string = date('Y-m-d H:i:s', mktime($date_array['hour'], $date_array['minute'], $date_array['second'], $date_array['month'], $date_array['day'], $date_array['year']));
-
-            $student->adm_date = $date_string;
-            $student->reg_no = $r[3];
-            $student->gender = ($r[4] == "Male") ? "M" : ($r[4] == "M") ? "M" : "F";
-            $student->status = 1;
-            $student->school_id = $this->organisation_info->id;
-            $student->others = "";
-            $student->current_class_id = $request->class_id;
-            $student->parent_id = 0;
-            $student->passport_photo = "";
-
-
-            
-        }
-        // return response()->json($data);
-        return response()->json([
-            'success' => true,
-            'data' => $data,
-            'records' => $students_added
-        ]);
-    }
-
-    public function postStudentRegister(Request $request){
-      $st = new Student;
-      $st->first_name = $request->first_name;
-      $st->second_name = $request->second_name;
-      $st->last_name = $request->last_name;
-      $st->dob = $request->dob;
-      $st->reg_date = $request->reg_date;
-      $st->student_gender = $request->student_gender;
-      $st->passport_photo = FileUpload::photo($request,'photo');
-      $st->student_school = $request->student_school;
-      if($st->save())
-      {
-        $student_id = $st->student_id;
-        Status::insert(['student_id'=>$student_id,'school_id'=>$request->school_id]);
-        return back();
-      }
+      // 
     }
 
     /**
@@ -153,21 +49,34 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
+      // validate the students form data
+      $this->validate($request, [
+        'regNum' => 'required|unique:students,',
+        'firstName' => 'required',
+        'secondName' => 'required',
+        'lastName' => 'required',
+        'gender' => 'required',
+        'dob' => 'required',
+        'school' => 'required',
+        'class' => 'required',
+        'parent' => 'required'
+      ]);
+
       // store students in the databse
       $student = $request->isMethod('put') ? Student::findOrFail($request->student_id) : new Student;
 
       $student->id = $request->input('student_id');
       
-      $student->student_first_name = $request->input('student_first_name');
-      $student->student_second_name = $request->input('student_second_name');
-      $student->student_last_surname = $request->input('student_last_surname');
-      $student->student_dob = $request->input('student_dob');
-      $student->student_gender = $request->input('student_gender');
-      $student->reg_date = $request->input('reg_date');
-      $student->student_gender = $request->input('student_gender');
-      $student->passport_photo = $request->input('passport_photo');
-      $student->student_school = $request->input('student_school');
-     
+      $student->student_reg = $request->input('regNum');
+      $student->class_id = $request->input('class');
+      $student->student_first_name = $request->input('firstName');
+      $student->student_second_name = $request->input('secondName');
+      $student->student_last_surname = $request->input('lastName');
+      $student->student_dob = $request->input('dob');
+      $student->student_gender = $request->input('gender');
+      $student->student_school = $request->input('school');
+      $student->student_parent = $request->input('parent');
+      $student->passport_photo = $request->input('photo');
 
       if ($student->save()) {
         return new StudentResource($student);
@@ -182,50 +91,17 @@ class StudentController extends Controller
      */
     public function show($id)
     {
-      $data = [];
-        $data["student"] = Student::find($id);
-        $data["results"] = self::getStudentResults($id);
-        $data["teacher"] = SchoolTeacher::where('student_id', $data["student"]->id)->get()->first();
-        if ($id != -1) {
+      $studentdetails = [];
 
-        }
-        return new StudentResource($student);
-    }
+      // get a single student
+      $studentdetails['student'] = Student::findOrFail($id);
 
-      public function getStudent($id)
-    {
-        $student = Student::find($id);
-        $student->parent = SchoolParent::where('student_id', '=', $id)->get()->first();
+      // get student results
 
-        return response()->json($student);
-    }
-
-     private function getTeacherStudents()
-    {
-        $user = Auth::user();
-        $teacher = Teacher::where('user_id', $user->id)->get()->first();
-
-        $roles = [];
-        $class_teacher = Stream::where('class_teacher', $teacher->id)->get(['id']);
-        $subject_teacher = SubjectTeacher::where('teacher_id', $teacher->id)->get(['class_id']);
-
-        foreach ($class_teacher as $item) {
-            array_push($roles, $item);
-        }
-        foreach ($subject_teacher as $item) {
-            //  array_push($roles,$item);
-        }
-        /*  array_push($roles,$class_teacher);
-          array_push($roles,$subject_teacher);*/
-
-        // return $roles;
-       
-        $students = Student::whereIn('teacher_class_id', $class_teacher)
-            ->orWhereIn('teacher_class_id', $subject_teacher)
-            ->orWhereIn('id', $members)
-            ->get();
-
-        return $students;
+      // get student parent
+      
+      // return a json response
+      return response()->json(['student' => $studentdetails], 200);
     }
 
     /**
@@ -236,7 +112,7 @@ class StudentController extends Controller
      */
     public function edit($id)
     {
-        //
+      //
     }
 
     /**
@@ -248,195 +124,8 @@ class StudentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        
-        $student = Student::find($id);
-        if ($student == null) {
-            return response("Could not update. No record found", 500);
-        }
-       if ($student->reg_no == $request->reg_no) {
-            $this->validate($request, [
-            'first_name' => 'required',
-            'reg_no' => 'required|unique:students,reg_no,NULL,id,organisation_info,' . $this->organisation_info->id,
-            'surname' => 'required',
-            'gender' => 'required',
-            'dob' => 'required',
-            'reg_date' => 'required',
-            'student_gender' => 'required',
-            'passport_photo' => 'required',
-            'student_school' => 'required'
-            ]);
-        } else {
-            $this->validate($request, [
-            'first_name' => 'required',
-            'reg_no' => 'required|unique:students,reg_no,NULL,id,organisation_info,' . $this->organisation_info->id,
-            'surname' => 'required',
-            'gender' => 'required',
-            'dob' => 'required',
-            'reg_date' => 'required',
-            'student_gender' => 'required',
-            'passport_photo' => 'required',
-            'student_school' => 'required'
-            ]);
-        }
-
-           $student->student_first_name = $request->input('student_first_name');
-          $student->student_second_name = $request->input('student_second_name');
-          $student->student_last_surname = $request->input('student_last_surname');
-          $student->student_dob = $request->input('student_dob');
-          $student->student_gender = $request->input('student_gender');
-          $student->reg_date = $request->input('reg_date');
-          $student->student_gender = $request->input('student_gender');
-          $student->passport_photo = $request->input('passport_photo');
-          $student->student_school = $request->input('student_school');
-          $student->save();
-
-        $parent = SchoolParent::where('student_id', '=', $student->id)->get()->first();
-        if ($parent == null) {
-            $parent = SchoolParent::create([
-              'first_name' => $request->first_name,
-              'second_name' => $request->second_name,
-              'national_id' => $request->national_id,
-              'phone_number' => $request->phone_number,
-              'email' => $request->email,
-              'county'=>$request->county,
-              'sub_county'=>$request->sub_county,
-              'ward'=>$request->ward
-
-            ]);
-        } else {
-            
-              $parent->first_name = $request->input('first_name');
-              $parent->second_name = $request->input('second_name');
-              $parent->national_id = $request->input('national_id');
-              $parent->phone_number = $request->input('phone_number');
-              $parent->email = $request->input('email');
-              $parent->parent_id = $request->input('parent_id');
-              $parent->county = $request->input('county');
-              $parent->sub_county = $request->input('sub_county');
-              $parent->ward = $request->input('ward');
-               $parent->save();
-        }
+      // 
     }
-
-     private static function getStudentSubjects($id)
-    {
-        $studentSubjects = [];
-        $subjects = StudentSubject::where('student_id', '=', $id)->orderBy('id', 'desc')->get();
-
-        foreach ($subjects as $subject) {
-            $subject->name = Subject::find($subject->id)->subject;
-            array_push($studentSubjects, $subject);
-        }
-        $student = Student::find($id);
-
-        $schoolSubjects = Subject::where('school_id', $student->school_id)
-            ->where('is_compulsory', true)
-            ->get();
-        foreach ($schoolSubjects as $schoolSubject) {
-            $studentSubject = new StudentSubject();
-            $studentSubject->id = -1;
-            $studentSubject->student_id = $id;
-            $studentSubject->subject_id = $schoolSubject->id;
-            $studentSubject->name = $schoolSubject->subject;
-
-            array_push($studentSubjects, $studentSubject);
-        }
-
-        return $studentSubjects;
-    }
-
-     public function studentRecords()
-    {
-        $user = Auth::user();
-        $data = [];
-        if ($user->user_type == 2) {
-            $teacher = Teacher::where('user_id', $user->id)->get()->first();
-            if ($teacher == null) {
-                return response()->json([
-
-                ]);
-            }
-            $records = StudentRecord::where('teacher_id', $teacher->id)->get();
-            $data['records'] = $records;
-            $data['students'] = $this->getTeacherStudents();
-            return view('dashboard.pages.students.view-student-records', $data);
-        }
-        $data['records'] = [];
-        $data['students'] = Student::where('school_id', $this->organisation_info->id)->orderBy('id', 'desc')->get();
-
-        return view('views/students/studentcomponent.vue', $data);
-
-    }
-
-    public function createStudentRecord(Request $request)
-    {
-        $user = Auth::user();
-        $teacher = Teacher::where('user_id', $user->id)->get()->first();
-
-        if ($teacher == null) {
-            return response()->json([
-                'success' => false,
-            ]);
-        }
-        $record = StudentRecord::create([
-            'student_id' => $request->student_id,
-            'teacher_id' => $teacher->id,
-            'description' => $request->description,
-            'date' => new \DateTime($request->date),
-            'type' => $request->type,
-            'status' => Constants::ACTIVE_RECORD
-        ]);
-        $record->student = Student::find($request->student_id);
-        $record->date = $request->date;
-
-        try {
-            $record->teacher = $teacher;
-            $profile = new TargetProfile($request->student_id, '', Constants::STUDENT);
-            $data = new PushNotificationMessage();
-            $data->type = 'RECORD';
-            $data->target = $profile->id;
-            $data->data = $record;
-            $this->pushNotification($profile, $data);
-        } catch (\Exception $e) {
-
-        }
-
-        return response()->json([
-            'success' => true,
-            'data' => $record
-        ]);
-    }
-
-    public function updateStudentRecord(Request $request, $id)
-    {
-        $record = StudentRecord::find($id);
-        if ($record == null) {
-            return APIResponse::failResponse('No Record found');
-        }
-        $record->student_id = $request->student_id;
-        $record->description = $request->description;
-        $record->date = new \DateTime($request->date);
-        $record->type = $request->type;
-        $record->save();
-        try {
-            $teacher = Teacher::find($record->teacher_id);
-            $record->teacher = $teacher;
-            $profile = new TargetProfile($request->student_id, '', Constants::STUDENT);
-            $data = new PushNotificationMessage();
-            $data->type = 'RECORD';
-            $data->target = $profile->id;
-            $data->data = $record;
-            $this->pushNotification($profile, $data);
-        } catch (\Exception $e) {
-
-        }
-        return response()->json([
-            'success' => true,
-            'data' => $record
-        ]);
-    }
-
-
 
     /**
      * Remove the specified resource from storage.
