aa4ac85fe045e259f8264525bdd8fac1ca858a35
Delink-D <delinkdesigns@gmail.com>
2018-04-28 22:13:50 +0300

    added pop for validation

--- resources/assets/js/views/users/add/UsersAddComponent.vue
+++ resources/assets/js/views/users/add/UsersAddComponent.vue
@@ -3,40 +3,40 @@
     <h3>Add Users</h3>
 
     <!-- form starts -->
-    <el-form ref="form" :model="form" :rules="rules">
+    <el-form ref="userForm" :model="userForm" :rules="rules" status-icon>
       <el-col :span="11">
-        <el-form-item label="First Name">
-          <el-input v-model="form.firstname" size="medium"></el-input>
+        <el-form-item label="First Name"  prop="firstname">
+          <el-input v-model="userForm.firstname" size="medium" placeholder="First Name"></el-input>
         </el-form-item>
       </el-col>
       <el-col :span="2">&nbsp;</el-col>
       <el-col :span="11">
-        <el-form-item label="Last Name">
-          <el-input v-model="form.lastname" size="medium"></el-input>
+        <el-form-item label="Last Name" prop="lastname">
+          <el-input v-model="userForm.lastname" size="medium"></el-input>
         </el-form-item>
       </el-col>
 
       <el-col :span="11">
-        <el-form-item label="User Name">
-          <el-input v-model="form.username" size="medium"></el-input>
+        <el-form-item label="User Name" prop="username">
+          <el-input v-model="userForm.username" size="medium"></el-input>
         </el-form-item>
       </el-col>
       <el-col :span="2">&nbsp;</el-col>
       <el-col :span="11">
-        <el-form-item label="E-mail">
-          <el-input v-model="form.email" size="medium"></el-input>
+        <el-form-item label="E-mail" prop="email">
+          <el-input v-model="userForm.email" size="medium"></el-input>
         </el-form-item>
       </el-col>
 
       <el-col :span="24">
-        <el-form-item label="Phone Number">
-          <el-input v-model="form.phone" size="medium"></el-input>
+        <el-form-item label="Phone Number" prop="phone">
+          <el-input v-model="userForm.phone" size="medium"></el-input>
         </el-form-item>
       </el-col>
       
       <el-col :span="7">
-        <el-form-item label="School">
-          <el-select v-model="form.school" placeholder="Please select school" size="medium">
+        <el-form-item label="School" prop="school">
+          <el-select v-model="userForm.school" placeholder="Please select school" size="medium">
             <el-option label="School one" value="1"></el-option>
             <el-option label="School two" value="2"></el-option>
           </el-select>
@@ -44,8 +44,8 @@
       </el-col>
       <el-col :span="1">&nbsp;</el-col>
       <el-col :span="7">
-        <el-form-item label="User Level">
-          <el-select v-model="form.level" placeholder="Please user level" size="medium">
+        <el-form-item label="User Level" prop="level">
+          <el-select v-model="userForm.level" placeholder="Please user level" size="medium">
             <el-option label="Head Master" value="head"></el-option>
             <el-option label="Teacher" value="teacher"></el-option>
             <el-option label="Sec" value="sec"></el-option>
@@ -54,14 +54,14 @@
       </el-col>
       <el-col :span="2">&nbsp;</el-col>
       <el-col :span="7">
-        <el-form-item label="Teacher?">
-          <el-switch v-model="form.hasTeacherObject"></el-switch>
+        <el-form-item label="Teacher?" prop="hasTeacherObject"> <br>
+          <el-switch v-model="userForm.hasTeacherObject"></el-switch>
         </el-form-item>
       </el-col>
 
       <el-col :span="24">
-        <el-form-item label="User Roles">
-          <el-checkbox-group v-model="form.roles">
+        <el-form-item label="User Roles" prop="roles">
+          <el-checkbox-group v-model="userForm.roles">
             <el-checkbox label="Admin" name="type"></el-checkbox>
             <el-checkbox label="Head Master" name="type"></el-checkbox>
             <el-checkbox label="Dean" name="type"></el-checkbox>
@@ -71,8 +71,8 @@
       </el-col>
 
       <el-col :span="11">
-        <el-form-item label="Password" prop="pass">
-          <el-input type="password" v-model="form.password" size="medium" auto-complete="off"></el-input>
+        <el-form-item label="Password" prop="password">
+          <el-input type="password" v-model="userForm.password" size="medium" auto-complete="off"></el-input>
         </el-form-item>
       </el-col>
       <el-col :span="2">&nbsp;</el-col>
