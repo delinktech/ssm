fe4136b0cd432d5f22558d43d3b67154b813afdb
Delink-D <delinkdesigns@gmail.com>
2018-04-29 05:26:32 +0300

    included all the fields in the model

--- app/Models/School.php
+++ app/Models/School.php
@@ -10,7 +10,7 @@ class School extends Model
 
     protected $primaryKey='id';
 
-    protected $fillable=['school_name' ,'reg_no', 'school_logo' , 'school_address', 'school_mobile', 'school_email', 'school_county', 'school_town', 'school_level', 'user_id'];
+    protected $fillable=['school_name' ,'reg_no', 'school_logo' , 'school_address', 'school_mobile', 'school_email', 'school_county', 'school_town', 'school_level', 'user_id', 'createdBy', 'updatedBy'];
 
     public function studentSchool(){
       return $this->hasMany('App\Models\Student');
