003c98bd93fc255f953b8ba785d6790ed01945c7
Delink-D <delinkdesigns@gmail.com>
2018-04-29 05:27:02 +0300

    removed un-used code from schools controller

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use JWTAuth;
 use App\Models\School;
+use App\Models\Teacher;
+use App\Models\ClassModel;
+use App\Models\Student;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -29,40 +33,7 @@ class SchoolController extends Controller
    */
   public function create()
   {
-    $this->validate($request, [
-      'school_code' => 'required',
-      'school_name' => 'required',
-      'school_reg_number' => 'required',
-      'school_logo' => 'required',
-      'school_level' => 'required',
-      'school_type' => 'required',
-      'school_gender' => 'required',
-      'school_county' => 'required',
-      'school_sub_county' => 'required',
-      'school_ward' => 'required',
-      'school_phone' => 'required',
-      'school_email' => 'required'
-      ]);
-
-    $school = School::create([
-      'school_code' => $request->school_code,
-      'school_name' => $request->school_name,
-      'school_reg_number' => $request->school_reg_number,
-      'school_logo' => $request->school_logo,
-      'school_level' => $request->school_level,
-      'school_type' => $request->school_type,
-      'school_gender' => $request->school_gender,
-      'school_county' => $request->school_county,
-      'school_sub_county' => $request->school_sub_county,
-      'school_ward' => $request->school_ward,
-      'school_phone' => $request->school_phone,
-      'school_email' => $request->school_email
-      ]);
-
-    return response()->json([
-      'success' => true,
-      'id' => $school->id
-      ]);
+    // 
   }
 
   /**
@@ -73,14 +44,28 @@ class SchoolController extends Controller
    */
   public function store(Request $request)
   {
+    // validate the school post data
+    $this->validate($request, [
+      'school_code' => 'required|unique:schools',
+      'school_name' => 'required',
+      'school_level' => 'required',
+      'school_type' => 'required',
+      'school_gender' => 'required',
+      'school_county' => 'required',
+      'school_sub_county' => 'required',
+      'school_ward' => 'required',
+      'school_phone' => 'required',
+      'school_email' => 'required',
+      'school_logo' => 'required'
+      ]);
+
     // store school in the database
     $school = $request->isMethod('put') ? School::findOrFail($request->school_id) : new School;
+
     $school->id = $request->input('school_id');
 
     $school->school_code = $request->input('school_code');
     $school->school_name = $request->input('school_name');
-    $school->school_reg_number = $request->input('school_reg_number');
-    $school->school_logo = $request->input('school_logo');
     $school->school_level = $request->input('school_level');
     $school->school_type = $request->input('school_type');
     $school->school_gender = $request->input('school_gender');
@@ -89,12 +74,15 @@ class SchoolController extends Controller
     $school->school_ward = $request->input('school_ward');
     $school->school_phone = $request->input('school_phone');
     $school->school_email = $request->input('school_email');
+    $school->school_logo = $request->input('school_logo');
+    $school->createdBy = JWTAuth::parseToken()->toUser()->username;
+    $school->updatedBy = JWTAuth::parseToken()->toUser()->username;
 
     if ($school->save()) {
       // return a json response
       return response()->json([
         'success' => true,
-        'obj_saved' => $school
+        'school' => $school
         ], 200);
     }
   }
@@ -107,14 +95,22 @@ class SchoolController extends Controller
    */
   public function show($id)
   {
-    $data = [];
-    $data["school"] = Scchool::find($id);
-    $data["teacher"] = SchoolTeacher::where('teacher_id', $data["teacher"]->id)->get()->first();
-    if ($id != -1) {
+    $schoolinfo = [];
+    // get school data
+    $schoolinfo["school"] = School::find($id);
 
-    }
-    
-    return new SchoolResource($school);
+    // get teachers count
+    $schoolinfo["teachers"] = Teacher::where('teacher_school_id', $schoolinfo['school']->id)->count();
+
+    // get teachers count
+    $schoolinfo["classes"] = ClassModel::where('school', $schoolinfo['school']->id)->count();
+
+    // get students count
+    $schoolinfo["students"] = Student::where('student_school', $schoolinfo['school']->id)->count();
+
+    return response()->json([
+      'school' => $schoolinfo
+      ], 200);
   }
 
   /**
@@ -125,7 +121,7 @@ class SchoolController extends Controller
    */
   public function edit($id)
   {
-      //
+    //
   }
 
   /**
@@ -137,7 +133,7 @@ class SchoolController extends Controller
    */
   public function update(Request $request, $id)
   {
-      //
+    //
   }
 
   /**
@@ -148,16 +144,14 @@ class SchoolController extends Controller
   */
   public function destroy($id)
   {
-
     // delete school
     $school = School::findOrFail($id);
 
     if ($school->delete()) {
-
       // return a json response
       return response()->json([
         'success' => true,
-        'obj_deleted' => $school
+        'school' => $school
       ], 200);
     }
   }
