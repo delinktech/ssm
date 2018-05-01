9697c4af024092633c149944efff2a2582cef0b2
Delink-D <delinkdesigns@gmail.com>
2018-05-01 20:30:43 +0300

    added the request url for class

---
+++ resources/assets/js/api/class.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import { getToken } from '../utils/auth'
+
+Vue.use(VueResource)
+
+/* fetch all Schools function via api */
+export const getClasses = () => {
+  return Vue.http.get('/api/classes', { params: { 'token': getToken() }} )
+}
+
+/* fetch single School function via api */
+export const getClass = (_id) => {
+  return Vue.http.get('/api/class', { params: { 'id': _id, 'token': getToken() }} )
+}
+
+/* save Schools through the api */
+export const saveClass = (formData) => {
+  return Vue.http.post('/api/class', formData, { params: { 'token': getToken() }} )
+}
+
+/* save edits of Schools through the api */
+export const saveEditClass = (formData) => {
+  return Vue.http.put('/api/class', formData, { params: { 'token': getToken() }} )
+}
+
+/* delete a Schools through the api */
+export const deleteClass = (_id) => {
+  return Vue.http.delete('/api/class', { params: { 'id': _id, 'token': getToken() }} )
+}
