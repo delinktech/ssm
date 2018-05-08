20b3c4f938d31ccf40b81ebba80deac02bd5a407
Unknown <kamaunewton78@gmail.com>
2018-05-08 09:35:51 +0300

    Email view
    kindly create the view as per Delink Standards and the email content as per the same

---
+++ resources/assets/js/views/email/SchoolRegistrationComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+    <h3>Email</h3>
+    <router-view>Create Email view and content</router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
