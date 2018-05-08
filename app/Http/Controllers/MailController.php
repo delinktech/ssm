f3e279b006610cf56f3a27e09a0795253748cd2f
Unknown <kamaunewton78@gmail.com>
2018-05-08 09:36:25 +0300

    email controller with send function

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+
+class MailController extends Controller
+{
+    public function send()
+    {
+    	Mail::send(['text'=>'mail'],['name','Delink'],function($message){
+    		$message->to('kamaunewton78@gmail.com', 'To Kamau')->subject('Test Results');
+    		$message->from('schoolregistrar@gmail.com', 'School');
+    	});
+    }
+}
