5bc65b3fc90d770b96bcfe69b132ee3880e7d8ce
Delink-D <delinkdesigns@gmail.com>
2018-05-12 13:06:21 +0300

    added linechart on the dashboard

--- resources/assets/js/views/dashboard/index.vue
+++ resources/assets/js/views/dashboard/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="dashboard-container">
+    <school-info></school-info>
+
+    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+      <line-chart :chart-data="lineChartData"></line-chart>
+    </el-row>
+
     <div class="dashboard-text">name:{{name}}</div>
     <div class="dashboard-text">roles:<span v-for='role in roles' :key='role'>{{role}}</span></div>
     <div class="dashboard-text">school: {{school.school_name}}</div>
@@ -8,15 +14,50 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import SchoolInfo from './components/SchoolInfo'
+import LineChart from './components/LineChart'
+
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
 
 export default {
   name: 'dashboard',
+  components: {
+    SchoolInfo,
+    LineChart
+  },
+  data() {
+    return {
+      lineChartData: lineChartData.newVisitis
+    }
+  },
   computed: {
     ...mapGetters([
       'name',
       'roles',
       'school'
     ])
+  },
+  methods: {
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData[type]
+    }
   }
 }
 </script>
