f01276a3b21af783872aee94d4d242430129a309
Delink-D <delinkdesigns@gmail.com>
2018-05-19 03:02:39 +0300

    setup of a dean approve component

---
+++ resources/assets/js/views/results/dean/AllResutsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-container">
+    <grouped-results></grouped-results>
+  </div>
+</template>
+
+<script>
+  import GroupedResults from './components/GroupedResults'
+
+  export default {
+    name: 'AllResutsComponent',
+    components: {
+      GroupedResults
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/views/results/dean/components/GroupedResults.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <el-col :span="7" v-for="o in 40" :key="o" :offset="offset" class="my-card">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span style="line-height: 36px;">Card name</span>
+        </div>
+        <div>
+        <p>Form 1 Boys room 1</p>
+        </div>
+
+        <div>
+          <span>Send Notifictions</span>
+          <div class="bottom clearfix">
+            <time class="time">{{ currentDate }}</time>
+            <el-button type="text" class="button">Send</el-button>
+          </div>
+        </div>
+      </el-card>
+    </el-col>
+  </div>
+</template>
+
+<script>
+  import moment from 'moment'
+
+  export default {
+    name: 'GroupedResults',
+    data() {
+      return {
+        offset: 1,
+        currentDate: moment(new Date()).format('dddd MMM DD-YYYY')
+      };
+    }
+  }
+</script>
+
+<style scoped>
+  .my-card {
+    margin-bottom: 4%;
+  }
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+  .button {
+    padding: 0;
+    float: right;
+  }
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+  .clearfix:after {
+      clear: both
+  }
+</style>
