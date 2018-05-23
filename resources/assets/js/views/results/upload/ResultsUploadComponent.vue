b8b533d957e6d38dce917ffb1155f8e7e16ce284
Delink-D <delinkdesigns@gmail.com>
2018-05-23 22:45:04 +0300

    added results fields

--- resources/assets/js/views/results/upload/ResultsUploadComponent.vue
+++ resources/assets/js/views/results/upload/ResultsUploadComponent.vue
@@ -3,9 +3,40 @@
     <!-- upload excel files here -->
     <upload-excel-component @on-selected-file='selected'></upload-excel-component>
 
+    <!-- display form -->
+    <el-form ref="resultsForm" :model="resultsForm" :rules="resultsRules" status-icon>
+      <el-col :span="7">
+        <el-form-item label="Term" prop="term">
+          <el-select v-model="resultsForm.term" placeholder="Please results term" size="medium">
+            <el-option v-for="term in 3" :key="term" :value="term">Term {{term}}</el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="1">&nbsp;</el-col>
+      <el-col :span="7">
+        <el-form-item label="User Level" prop="cls">
+          <el-select v-model="resultsForm.cls" placeholder="Select Class" size="medium">
+            <el-option label="Admin" value="admin"></el-option>
+            <el-option label="Head Master" value="head"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="2">&nbsp;</el-col>
+      <el-col :span="7">
+        <el-form-item label="Class Teacher" prop="teacher">
+          <el-select v-model="resultsForm.teacher" placeholder="Pick class Teacher" size="medium">
+            <el-option label="Admin" value="admin"></el-option>
+            <el-option label="Head Master" value="head"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+    </el-form>
+
+    <!-- button to upload results -->
     <el-button @click="getData(tableData)" type="primary" circle id="fab-upload">
       <i class="el-icon-upload2"></i>
     </el-button>
+
     <!-- display data on this table -->
     <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
       <el-table-column v-for='item of tableHeader' :prop="item" :label="item" :key='item'>
