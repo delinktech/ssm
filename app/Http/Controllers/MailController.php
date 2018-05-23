a9c1a5898fc3d3bcf70ad689c1b2ffde02632683
Delink-D <delinkdesigns@gmail.com>
2018-05-23 22:05:52 +0300

    reorganized the email function

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,19 +10,53 @@ use Mail;
 
 class MailController extends Controller
 {
-  /**
-  * function to send emails
-  */
-  public function send()
-  {
-    Mail::send('emails.send-results',['name','Delink'],function($message){
-      $message
-        ->to('delinkdesigns@gmail.com', 'To Kamau')
-        ->subject('Test Results')
-        ->from('schoolregistrar@gmail.com', 'School');
-    });
-  }
 
+    public function html_email() {
+      // call function to send mail
+      sendMail();
+      foreach ($variable as $key => $value) {
+        # code...
+      }
+    }
+
+    /**
+     * function to send email
+     */
+    private function sendEmail() {
+      $data = array('name'=>"Virat Gandhi");
+      Mail::send('emails.results-mail', $data, function($message) {
+        $message->to('ngichngimwa@gmail.com', 'Tutorials Point')
+                ->subject('Laravel HTML Testing Mail')
+                ->from('delinkdeveloper@gmail.com','Virat Gandhi');
+      });
+      echo "HTML Email Sent. Check your inbox.";
+    }
+
+    /*
+    public function basic_email(){
+      $data = array('name'=>"Virat Gandhi");
+      Mail::send(['text'=>'emails.results-mail'], $data, function($message) {
+			$message->to('ngichngimwa@gmail.com', 'Tutorials Point')
+							->subject('Laravel Basic Testing Mail')
+							->from('kamaunewton78@gmail.com','Virat Gandhi');
+		});
+		echo "Basic Email Sent. Check your inbox.";
+	}
+
+	public function attachment_email(){
+		$data = array('name'=>"Virat Gandhi");
+		Mail::send('emails.results-mail', $data, function($message) {
+			$message->to('kamaunewton78@gmail.com', 'Tutorials Point')->subject
+			('Laravel Testing Mail with Attachment');
+			$message->attach('C:\laravel-master\laravel\public\uploads\image.png');
+			$message->attach('C:\laravel-master\laravel\public\uploads\test.txt');
+			$message->from('kamaunewton78@gmail.com','Virat Gandhi');
+		});
+		echo "Email Sent with attachment. Check your inbox.";
+  }
+  */
+
+  // TODO: start using thirdparty mail delivery services
   // public function sendMail($value='')
   // {
   //   Mail::send('emails.send-results', [], function ($message) {
