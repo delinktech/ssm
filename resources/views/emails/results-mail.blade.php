aec4941c198488154b226970ed38cfd26b732571
Delink-D <delinkdesigns@gmail.com>
2018-05-23 22:05:26 +0300

    emails blade

---
+++ resources/views/emails/results-mail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Student Results</title>
+  </head>
+  <body>
+    <h1>{{$name}}</h1>
+
+    <p>Good Day</p>
+
+    <p>Yours,</p>
+    <p>Delink_</p>
+  </body>
+</html>
