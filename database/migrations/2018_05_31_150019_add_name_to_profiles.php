039199f5921bb4326064ebd58c49cf5f6afc0dc7
Delink-D <delinkdesigns@gmail.com>
2018-06-02 15:40:38 +0300

    added migration to update existing table without loosing data

---
+++ database/migrations/2018_05_31_150019_add_name_to_profiles.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNameToProfiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->string('name',20)->after('user_id');
+            $table->string('news',20)->after('name');
+          });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('news');
+          });
+    }
+}
